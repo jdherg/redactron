e0eb9273de08ed920d6c0c5432b9b1c241366e99
John Hergenroeder <jdherg@gmail.com>
2014-12-11 10:40:09 -0600

    Refactored to allow redaction and target method selection. (Thanks for pairing, @davidbalbert !)

--- redactron.py
+++ redactron.py
@@ -9,14 +9,36 @@ def redact_word(word):
     return word[0] + 'x' * (len(word) - 1)
 
 
-def redact_phrase(phrase, redactions):
-    result = phrase
-    words = set(re.split('\W+', phrase))
-    in_phrase = words & redactions
+def reverse_word(word):
+    return word[::-1]
+
+
+def redact_positionally(text, positions, redaction_method=redact_word):
+    for position in positions:
+        (start, stop) = position
+        text = text[:start] + redaction_method(text[start:stop]) + \
+            text[stop:]
+    return text
+
+
+def redact_set(text, redaction_set, redaction_method=redact_word):
+    words = set(re.split('\W+', text))
+    in_phrase = words & redaction_set
     for redaction in in_phrase:
-        result = re.sub('\\b' + redaction + '\\b',
-                        redact_word(redaction), result)
-    return result
+        text = re.sub('\\b' + redaction + '\\b',
+                      redaction_method(redaction), text)
+    return text
+
+
+def redact_properish(text, redaction_method=redact_word):
+    words = set(re.split('\W+', text))
+    upcase = {word for word in words if len(word) and word[0].isupper()}
+    only_up = {word for word in upcase if word.lower() not in words}
+    return redact_set(text, only_up)
+
+
+def redact(text, target_method=redact_properish, redaction_method=redact_word):
+    return target_method(text, redaction_method=redaction_method)
 
 
 def main():
@@ -26,14 +48,7 @@ def main():
     parser.add_argument('output_file', type=argparse.FileType('w'),
                         nargs='?', default=sys.stdout)
     args = parser.parse_args()
-    lines = args.input_file.readlines()
-    words = set()
-    for line in lines:
-        words |= set(re.split('\W+', line))
-    upcase = [word for word in words if len(word) and word[0].isupper()]
-    only_up = [word for word in upcase if word.lower() not in words]
-    for line in lines:
-        print(redact_phrase(line, set(only_up)), file=args.output_file, end='')
-
+    input_text = args.input_file.read()
+    print(redact(input_text), file=args.output_file)
 if __name__ == '__main__':
     main()
